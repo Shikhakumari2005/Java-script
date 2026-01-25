018ce8f7064ca6d441a356f956d4e1bd4fcc9ac1
unknown <siragenius1@gmail.com>
2026-01-25 18:45:23 +0530

    first js

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Krisnathesuoreme leader")
+const accid=123
+let name="gotali"
+var email="kris@gamil"
+var city="benguluare"
+let state;
+console.table([accid,name,email,city,state])git init
+
