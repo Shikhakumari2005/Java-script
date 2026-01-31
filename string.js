eee0154e137bbb93488d41662650a519a696bc26
unknown <siragenius1@gmail.com>
2026-01-31 16:01:24 +0530

    Stringjs

---
+++ string.js
@@ -0,0 +1,6 @@
+//for deep knowlege study the document like trimejson moxild
+const name="sikhasha"
+console.log(name);
+const age=90;
+console.log(name +age)
+console.log(`hello my name is shri rada rani ${name} and my daughteris ${age}`)
--- test.js
+++ test.js
@@ -4,9 +4,23 @@ let name="gotali"
 var email="kris@gamil"
 var city="benguluare"
 let state;
-console.table([accid,name,email,city,state])git init
+console.table([accid,name,email,city,state])
 
-console.log("1" +2)
-console.log(2+"1");
-console.log("1"+2+2);
-console.log(2+2+"1");
+
+
+//opertionlect7
+console.log("1" +2)       //stor as string
+console.log(2+"1");         //same string
+console.log("1"+2+2);        //same string
+console.log(2+2+"1");         //41 tstore  as int
+let  game=100;
+++game;
+console.log(game)
+game++
+console.log(game)
+
+//prefix,postfix
+//comparison
+console.log(null>0)
+console.log(null-0)
+console.log("2"===0)     //===conversion +datatype
